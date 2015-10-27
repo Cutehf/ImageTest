f1e50ea9143ba17b9b6efa449f836078074155f3
Cutehf <huangfei@guamudemac-mini.local>
2015-10-27 15:50:33 -0700

    1.0修改

---
+++ ImageView/ImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageView
+//
+//  Created by 瓜木 on 15/10/27.
+//  Copyright © 2015年 黄飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
